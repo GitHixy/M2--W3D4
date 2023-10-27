da50e187a6dd62af4c09934219642f081d938505
GitHixy <michael.pecora.92@gmail.com>
2023-10-27 17:58:58 +0200

    try1

--- template.js
+++ template.js
@@ -15,10 +15,46 @@
 
 //ESERCIZI IN JS
 // ESERCIZIO 7: Scrivi una funzione per cambiare il contenuto del tag h1 in qualcos'altro
+
+const cambiaTitolo = function () {
+let h1Mod = document.querySelector("h1")
+h1Mod.textContent = "- Affiliate Shop -";
+}
+
 // ESERCIZIO 8: Scrivi una funzione per cambiare il colore di background della pagina
-// ESERCIZIO 9: Scrivi una funzione per cambiare l'indirizzo presente nel futuro in un altro, fittizio
+
+const cambiaColoreSfondo = function (colore) {
+    let sfondoPagina = document.querySelector("body")
+    sfondoPagina.style.backgroundColor = colore
+}
+
+// ESERCIZIO 9: Scrivi una funzione per cambiare l'indirizzo presente nel footer in un altro, fittizio
+
+const indirizzoMod = function () {
+    let indirizzo = document.querySelector(".indirizzo")
+    indirizzo.textContent = "Indirizzo: Riozzo (MI), via Briganti 45, 20070";
+}
+
 // ESERCIZIO 10: Scrivi una funzione per aggiungere una classe CSS ad ogni link Amazon della tabella
+
+const aggiungiClass = function () {
+    let tuttiGliA = document.querySelectorAll("a")
+    for (let i = 0; i < tuttiGliA.length; i++) {
+        let singoloA = tuttiGliA[i]
+    singoloA.classList.add("classe-link")
+    }
+}
+
 // ESERCIZIO 11: Scrivi una funzione per aggiungere/togliere una classe CSS a tutte le immagini della tabella; questa classe deve modificare la visibilità/invisibilità dell'immagine
+
+const toggleImg = function() {
+    let tutteLeImg = document.querySelectorAll("img")
+    for (let i = 0; i < tutteLeImg.length; i++) {
+    let singolaImg = tutteLeImg[i]
+    singolaImg.classList.toggle("img-on-off")
+    }
+}
+
 // ESERCIZIO 12: Scrivi una funzione per cambiare il colore del prezzo di ogni prodotto in uno differente, ogni volta che viene invocata
 
 
